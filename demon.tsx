97c54424472746ae04a52136eae9c4ef75250bcc
christianclapson <38335453+christianclapson@users.noreply.github.com>
2018-04-19 14:41:15 +1000

    its pretty
    good

---
+++ demon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="demon " tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../0x72_16x16DungeonTileset.v3.png" width="256" height="256"/>
+</tileset>
